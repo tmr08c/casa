d4513a00dcd5167c14001a27f06291eae41bf583
Keith Bennett <keithrbennett@gmail.com>
2020-09-06 16:18:18 -0400

    Create UI for creating new CASA org but not yet working.

--- app/controllers/all_casa_admins/casa_orgs_controller.rb
+++ app/controllers/all_casa_admins/casa_orgs_controller.rb
@@ -1,5 +1,28 @@
 class AllCasaAdmins::CasaOrgsController < AllCasaAdminsController
+
   def show
     @casa_org = CasaOrg.find(params[:id])
   end
+
+  # GET /casa_orgs/new
+  def new
+    @casa_org = CasaOrg.new
+  end
+
+  # POST /casa_orgs
+  # POST /casa_orgs.json
+  def create
+    @casa_org = CasaOrg.new
+
+    respond_to do |format|
+      if @casa_org.save
+        format.html { redirect_to @casa_org, notice: "CASA organization was successfully created." }
+        format.json { render :show, status: :created, location: @casa_org }
+      else
+        format.html { render :new }
+        format.json { render json: @casa_org.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
